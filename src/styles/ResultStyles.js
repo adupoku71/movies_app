b20a5b07c37e7565ae3945846e355e4c95d5a517
adupoku71 <davidpoku71@gmail.com>
2021-11-23 10:08:09 +0000

    done with add page component
    added results card component

---
+++ src/styles/ResultStyles.js
@@ -0,0 +1,48 @@
+import styled from "styled-components"
+
+export const ResultContainer = styled.ul`
+  margin-top: 20px;
+`
+
+export const StyledResultCard = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`
+
+export const ResultInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  h3 {
+    font-size: 1.25rem;
+    color: #032541;
+    font-weight: 600;
+    margin: 0;
+  }
+
+  h4 {
+    font-size: 1.25rem;
+    font-weight: 300;
+    color: #032541;
+    margin: 0;
+  }
+`
+export const ResultImg = styled.img`
+  width: 75px;
+  height: 113px;
+  background-color: #dbdada;
+  border-radius: 5px;
+  margin-right: 15px;
+  display: block;
+  color: transparent;
+`
+export const ResultPoster = styled.div`
+  width: 75px;
+  height: 113px;
+  background-color: #dbdada;
+  border-radius: 5px;
+  margin-right: 15px;
+  display: block;
+  color: transparent;
+`
